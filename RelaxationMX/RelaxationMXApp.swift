8c0b9162ed9140f492f0dbcfcb8f4bcd0f60cfcd
Andrey Kudryavtsev <eightkbit@gmail.com>
2020-12-28 12:55:32 +0500

    Initial Commit

---
+++ RelaxationMX/RelaxationMXApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelaxationMXApp.swift
+//  RelaxationMX
+//
+//  Created by Andrey Kudryavtsev on 28.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct RelaxationMXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
